44664692f2d0c54b9c4b961bd7d44febd52711f1
zuzux3 <zuzannatarazewicz@gmail.com>
2021-10-11 11:13:39 +0200

    adding action

--- src/Start/MyFrame.java
+++ src/Start/MyFrame.java
@@ -5,17 +5,25 @@ import javax.swing.JFrame;
 //importowanie pakietu Swing, którego zawartoœci¹ jest JButton
 import javax.swing.JButton;
 
-public class MyFrame extends JFrame{
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MyFrame extends JFrame implements ActionListener{
 	public MyFrame() {
 		//wywo³anie przycisku
 		JButton btn = new JButton("Hello");
 		//dodawanie przycisku
 		this.add(btn);
-		System.out.println("Hi");
+		btn.addActionListener(this);
+		
 		//wyœwietlanie okienka
 		//ustawianie rozmiaru okienka
 		this.setSize(400,400);
 		//ustawianie widocznoœci okienka
 		this.setVisible(true);
 	}	
+	
+	public void actionPerformed(ActionEvent e) {
+		System.out.println("Hi");
+	}
 }
