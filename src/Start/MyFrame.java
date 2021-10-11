4f96251061fed9a37be2a8e4472cbb7aee10d2c4
zuzux3 <zuzannatarazewicz@gmail.com>
2021-10-11 11:03:08 +0200

    adding button

--- src/Start/MyFrame.java
+++ src/Start/MyFrame.java
@@ -1,9 +1,16 @@
 package Start;
+
 //importowanie pakietu Swing, którego zawartoœci¹ jest JFrame
 import javax.swing.JFrame;
+//importowanie pakietu Swing, którego zawartoœci¹ jest JButton
+import javax.swing.JButton;
 
 public class MyFrame extends JFrame{
 	public MyFrame() {
+		//wywo³anie przycisku
+		JButton btn = new JButton("Hello");
+		//dodawanie przycisku
+		this.add(btn);
 		System.out.println("Hi");
 		//wyœwietlanie okienka
 		//ustawianie rozmiaru okienka
