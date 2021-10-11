9e80f87918d847e44550cc6ef3bb768b31182f7e
zuzux3 <zuzannatarazewicz@gmail.com>
2021-10-11 10:57:08 +0200

    adding a visibility and size of a window

--- src/Start/MyFrame.java
+++ src/Start/MyFrame.java
@@ -1,11 +1,14 @@
 package Start;
-//importowanie pakietu Swing, którego zawartoœci¹ jest 
+//importowanie pakietu Swing, którego zawartoœci¹ jest JFrame
 import javax.swing.JFrame;
 
 public class MyFrame extends JFrame{
 	public MyFrame() {
 		System.out.println("Hi");
-	}
-	
-	
+		//wyœwietlanie okienka
+		//ustawianie rozmiaru okienka
+		this.setSize(400,400);
+		//ustawianie widocznoœci okienka
+		this.setVisible(true);
+	}	
 }
