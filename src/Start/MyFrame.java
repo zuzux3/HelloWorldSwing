af4d1f8d5b66892e66df7eded04f66ce00897c4c
zuzux3 <zuzannatarazewicz@gmail.com>
2021-10-12 21:33:36 +0200

    adding label and update comments
    label which prints "Hi" in program window and updating comments

--- src/Start/MyFrame.java
+++ src/Start/MyFrame.java
@@ -1,30 +1,45 @@
 package Start;
 
-//importowanie pakietu Swing, którego zawartoœci¹ jest JFrame
-import javax.swing.JFrame;
-//importowanie pakietu Swing, którego zawartoœci¹ jest JButton
-import javax.swing.JButton;
+//importowanie pakietu Swing, którego zawartoœci¹ jest JFrame, JButton i JLabel
+import javax.swing.*;
+//importowanie awt w celu u¿yciu m.in. BorderLayout
+import java.awt.*;
 //importowanie event w celu wykorzystania ActionEvent i ActionListener
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 
-public class MyFrame extends JFrame implements ActionListener{
+@SuppressWarnings("serial")
+public class MyFrame extends JFrame implements ActionListener {
+	//tworzenie ramki
+	JFrame frame = new JFrame("Hello World");
+	//tworzenie etykiety
+	JLabel label = new JLabel();
 	public MyFrame() {
 		//wywo³anie przycisku
 		JButton btn = new JButton("Hello");
 		//dodawanie przycisku
 		this.add(btn);
+		//tworzenie granicy przycisku, aby nie wyskakiwa³ na ca³¹ wielkoœæ okna
+		btn.setBounds(50, 150, 95, 30);
+		//dodawanie przycisku do ActionListener
 		btn.addActionListener(this);
 		
-		//wyœwietlanie okienka
+		//umieszczenie przycisku na dole okna
+		frame.getContentPane().add(BorderLayout.SOUTH, btn);
+		//umieszczanie napisu w œrodku po lewej stronie okna
+		frame.getContentPane().add(BorderLayout.CENTER, label);
+		//ustawianie zakoñczania 
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
 		//ustawianie rozmiaru okienka
-		this.setSize(400,400);
+		frame.setSize(400,400);
 		//ustawianie widocznoœci okienka
-		this.setVisible(true);
+		frame.setVisible(true);
 	}	
 	
 	//drukowanie "Hi" w konsoli
+	//@SuppressWarnings("deprecation")
 	public void actionPerformed(ActionEvent e) {
-		System.out.println("Hi");
+		//akcja - wyœwietlanie napisu Hi w oknie programu
+		label.setText("Hi!");
 	}
 }
